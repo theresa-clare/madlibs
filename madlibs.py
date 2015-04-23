ee6a0aa6279f5a7f782f748b786f7edfc1e2bd41
Hackbright Student <info@hackbrightacademy.com>
2015-04-23 15:48:06 -0700

    Beginning MadLibs, flask application links to goodbye and play game views.

---
+++ madlibs.py
@@ -0,0 +1,43 @@
+from random import choice
+
+from flask import Flask, render_template, request
+
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+# route to handle the landing page of a website.
+@app.route('/')
+def start_here():
+    return "Hi! This is the home page."
+
+# route to display a simple web page
+@app.route('/hello')
+def say_hello():
+    return render_template("hello.html")
+
+@app.route('/game')
+def show_game_form():
+    answer = request.args.get("playgame")
+    if answer == "yes":
+        return render_template("game.html")
+    else:
+        return render_template("goodbye.html")
+# @app.route('/greet')
+# def greet_person():
+#     player = request.args.get("person")
+
+#     AWESOMENESS = [
+#         'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
+#         'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
+
+#     compliment = choice(AWESOMENESS)
+
+#     return render_template("compliment.html", person=player, compliment=compliment)
+
+
+if __name__ == '__main__':
+    # debug=True gives us error messages in the browser and also "reloads" our web app
+    # if we change the code.
+    app.run(debug=True)
